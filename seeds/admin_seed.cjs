08bb50d08db457bdd2cd4ee9838e1180b82084ff
borascudan <borascu.dan24@gmail.com>
2025-04-25 01:49:13 +0300

    Schelete

---
+++ seeds/admin_seed.cjs
@@ -0,0 +1,12 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  await knex('admin').del()
+  await knex('admin').insert([
+    {name: 'Dan'},
+    {name: 'Florea'},
+    {name: 'Paunescu'}
+  ]);
+};
---
+++ src/routes/apiRoute.mjs
@@ -0,0 +1,6 @@
+import { Router } from "express";
+//import componenta from "./../endpoints/componenta";
+const router = Router()
+
+//router.use('/name/', componenta);
+export default router;
